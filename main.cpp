29f8f9f0f7f05cc4d9672ec9bf51eff4ed0e068b
TsaiDC <neko.artin@gmail.com>
2019-01-29 16:39:26 +0800

    remove macros

--- main.cpp
+++ main.cpp
@@ -5,15 +5,7 @@
 
 using namespace std;
 
-//#define DEBUG(fmt, args...) {printf("\033[1;32[DBG] %s(%d) %s : "fmt"\033[m", __FUNCTION__, __LINE__, __TIME__, ##args);}
-
-//#define debug_print(fmt, ...) \
-//    do { fprintf(stderr, "[DBG] %s(%d) %s : " fmt,  __FUNCTION__, __LINE__, __TIME__, __VA_ARGS__);} while(0)
-
-//#define TRACE(x) do{ debug_print x;} while(0)
-//#define TRACE(x) do{ printf("[DBG] %s(%d) %s : ", __FUNCTION__, __LINE__, __TIME__); printf(x);} while(0)
-//#define TRACE(fmt, ...) do{ printf("[DBG] %s(%d) %s : ", __FUNCTION__, __LINE__, __TIME__); printf(fmt, __VA_ARGS__);} while(0)
-    
+  
 void c_recur(int k, int n, int m, vector<int> list, bool isInit)
 {
     if(k-1 >=0) {
